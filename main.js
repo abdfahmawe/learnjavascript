640e3e6481c5ebd0ffaba54e97bcf20141d3e8a6
abdfahmawe <fahmawe09@gmail.com>
2024-09-11 18:26:41 +0300

    make after press in any buton before start the event it will return the fonts to it own natural size

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 function inc() {
+  document.querySelector("p").classList.remove("remove");
+  document.querySelector("h2").classList.remove("remove");
   document.querySelector("p").classList.add("add");
   document.querySelector("h2").classList.add("add");
 }
 function dec() {
+  document.querySelector("p").classList.remove("add");
+  document.querySelector("h2").classList.remove("add");
   document.querySelector("p").classList.add("remove");
   document.querySelector("h2").classList.add("remove");
 }
